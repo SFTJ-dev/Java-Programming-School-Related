af6f23f8ae3cdee43c7b3980c152f1dd618ecb85
Sofia Tabujara <tabujarasofia0@gmail.com>
2024-10-16 04:23:25 +0800

    Update ZooKeeper2.java

--- Java Zoo Project/Java Zoo Part II/ZooKeeper2.java
+++ Java Zoo Project/Java Zoo Part II/ZooKeeper2.java
@@ -1,1 +1,32 @@
-
+public class ZooKeeper2{ //start of the ZooKeeper2 class
+	private String name;
+	private String title;
+	private double payrate;
+	private boolean degree;
+	
+	//overloaded constructor for efficiency
+	//input all of the parameters inside the close and open parenthesis
+	public ZooKeeper2(String name, String title, double payrate, boolean degree){
+		this.name = name;
+		this.title = title;
+		this.payrate = payrate;
+		this.degree = degree;
+	}
+	
+	//getters
+	//if we already have the overloaded constructor
+	//then there is no need to set each parameters individually
+	
+	public String getName(){
+		return name;
+	}
+	public String getTitle(){
+		return title;
+	}
+	public double getPayrate(){
+		return payrate;
+	}
+	public boolean hasDegree(){
+		return degree;
+	} // end of the getters
+} // end of the ZooKeeper2 class
