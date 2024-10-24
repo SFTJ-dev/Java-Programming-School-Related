99295da0d49419f59b9eba5a0854e0e09874746c
Sofia Tabujara <tabujarasofia0@gmail.com>
2024-10-24 19:06:04 +0800

    Add files via upload

---
+++ Type Casting in Java/TypeCastingDemo.java
@@ -0,0 +1,17 @@
+public class TypeCastingDemo{
+	
+		public static void main(String[] args){
+			//local variables
+			
+		byte score = 10;
+		int totalScore = score;
+		
+		int price = 100;
+		float suggestedPrice = price;
+		
+		System.out.println("The value of total score is " + totalScore);
+		System.out.println("The value of suggested price is " + suggestedPrice);
+		
+		}
+		
+}
