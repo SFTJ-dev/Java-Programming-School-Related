4a3c9eaa73b07a5e5b52190ecdfacf3d9167f0e8
softhesoap <tabujarasofia0@gmail.com>
2024-11-20 10:41:18 +0800

    Update Main.java

--- Calculator OOP/Main.java
+++ Calculator OOP/Main.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class MainClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Compute compute = new Compute();
+
+        System.out.println("Enter numbers (or 0 to stop):");
+        int number;
+
+        while (true) {
+            System.out.print("Enter a number: ");
+            number = scanner.nextInt();
+
+            if (number == 0) break;
+
+            compute.addNumber(number);
+        }
+
+        System.out.println("\nResults:");
+        System.out.println("Sum: " + compute.getSum());
+        System.out.println("Subtraction: " + compute.getSubtraction());
+        System.out.println("Multiplication: " + compute.getProduct());
+        System.out.println("Division: " + compute.getDivision());
+
+        scanner.close();
+    }
+          }
