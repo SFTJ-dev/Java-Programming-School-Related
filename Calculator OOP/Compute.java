6a5bbedc4d842c997b4a00700c1439bd82478ccd
softhesoap <tabujarasofia0@gmail.com>
2024-11-20 11:29:18 +0800

    Update Compute.java

--- Calculator OOP/Compute.java
+++ Calculator OOP/Compute.java
@@ -1,41 +1,12 @@
 public class Compute {
-    private int sum = 0;
-    private int product = 1;
-    private int subtraction;
-    private double division;
-    private boolean firstNumber = true;
-
-    // Method to add a number to the calculations
-    public void addNumber(int number) {
-        if (number == 0) return;
-
-        // Initialize subtraction and division with the first number
-        if (firstNumber) {
-            subtraction = number;
-            division = number;
-            firstNumber = false;
-        } else {
-            subtraction -= number;
-            if (number != 0) division /= number;
-        }
-
-        sum += number;
-        product *= number;
+    public int add(int a, int b) {
+        return a + b;
     }
 
-    public int getSum() {
-        return sum;
+    public int subtract(int a, int b) {
+        return a - b;
     }
 
-    public int getProduct() {
-        return product;
-    }
+    public int multiply(int a, int b) {
 
-    public int getSubtraction() {
-        return subtraction;
-    }
-
-    public double getDivision() {
-        return division;
-    }
-}
+    
